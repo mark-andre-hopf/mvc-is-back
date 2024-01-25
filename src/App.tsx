91f535e58a612f9e7240e8660ee41087fb4103ae
Mark-André Hopf <mark-andre.hopf@metro.digital>
2024-01-25 18:07:56 +0100

    simplify code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Signal, signal } from '@preact/signals-react'
 import { useSignals } from '@preact/signals-react/runtime'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 //
@@ -15,7 +13,7 @@ const counter = signal(0)
 type Props = { model: Signal<number> }
 
 const CounterButton: React.FC<Props> = ({ model }) => (
-  <button onClick={() => ++model.value }>
+  <button onClick={() => ++model.value}>
     count is {model}
   </button>
 )
@@ -24,26 +22,9 @@ const App = () => {
   useSignals()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + SignalsX</h1>
-      <div className="card">
-        <CounterButton model={counter} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <CounterButton model={counter} />
+    </div>
   )
 }
 
